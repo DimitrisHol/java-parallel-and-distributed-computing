4b905ea81ec58327910d05ff0a04d992ccbfa171
Dimitris Hol <dimitris.hol11@gmail.com>
2018-05-27 17:41:47 +0300

    Assignment_3 Multiple Thread Solution

---
+++ Assignment_3/GroupThread.java
@@ -0,0 +1,48 @@
+class GroupThread extends Thread {
+
+  private int myId;
+  private int numThreads;
+  private double [] localSums;
+  private long numSteps;
+
+  private long myStart;
+  private long myStop;
+
+
+  //Constructor
+  public GroupThread(int threadId , int numThreads, double[] sums , long steps){
+
+    this.myId = threadId;
+    this.numThreads = numThreads;
+    this.localSums = sums;
+    this.numSteps = steps;
+
+    this.myStart = myId *  (numSteps / numThreads);
+    this.myStop = myStart + (numSteps / numThreads);
+    if (myId == (numThreads -1)) {
+      myStop = numSteps;
+    }
+  }
+
+  //Run
+  public void run(){
+
+    long startTime = System.nanoTime();
+
+    double sum = 0.0;
+    localSums[myId] = 0.0;
+
+
+    double step = 1.0 / (double)numSteps;
+    //Computation
+    for (long i = myStart; i < myStop; i++){
+      double x = ((double)i +0.5) * step;
+      sum += 1 / (1.0 + x*x);
+    }
+
+    localSums[myId] = 4 * sum * step;
+
+
+    System.out.printf("thread %d , Response time : %f seconds \n" ,myId , (double)(System.nanoTime() - startTime) / 1000000000);
+    }
+}
---
+++ Assignment_3/PiComputingThreads.java
@@ -0,0 +1,67 @@
+public class PiComputingThreads{
+
+public static void main(String[] args) {
+
+  long numSteps = 0;
+  int numThreads = 0;
+
+  //Parsing numSteps
+  if (args.length != 2) {
+    System.out.println("arguments:  number_of_steps");
+    System.exit(1);
+  }
+  try {
+    numSteps = Long.parseLong(args[0]);
+    numThreads = Integer.parseInt(args[1]);
+
+  } catch (NumberFormatException e) {
+    System.out.println("argument "+ args[0] +" must be long int");
+    System.exit(1);
+  }
+
+  // Get the number of available cores.
+  int cores = Runtime.getRuntime().availableProcessors();
+
+  //Set the number of threads to run.
+  // numThreads = cores;
+
+  //Cores are defined by user
+  System.out.println("cores = " + numThreads);
+
+  //Array of numSteps to store results coming from Threads.
+  double[] localSums = new double[numThreads];
+
+  //Make an array of type groupThread as many as the number of Threads.
+  GroupThread groups[] = new GroupThread[numThreads];
+
+  //Start counting time.
+  long startTime = System.nanoTime();
+
+  //Make the threads, and start them.
+  for (int i=0; i < numThreads; i++){
+    groups[i] = new GroupThread(i , numThreads, localSums , numSteps);
+    groups[i].start();
+  }
+
+    //Wait for all of them to finish.
+  for(int i = 0; i < numThreads; i++) {
+    try {
+      groups[i].join();
+            } catch (InterruptedException e) {}
+  }
+
+  double pi = 0.0;
+  for (int i=0; i< numThreads; i++){
+    pi += localSums[i];
+  }
+
+
+  System.out.printf("pararell program results with %d steps\n", numSteps);
+  System.out.printf("computed pi = %22.20f\n" , pi);
+  System.out.printf("difference between estimated pi and Math.PI = %22.20f\n", Math.abs(pi - Math.PI));
+
+
+  System.out.printf("Response time : %f seconds \n" , (double)(System.nanoTime() - startTime) / 1000000000);
+
+  }
+}
